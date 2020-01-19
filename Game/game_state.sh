3582141d910c7e5b6ec60e79aa479560808d9154
augie <8711710@gmail.com>
2020-01-19 13:48:21 -0800

    Minesweeper Prototype
    Initial design for command-line based Minesweeper game.
    
    Game's start sequence needs redesign.
    Following commits will contain ./Game/grid.sh and /.Game/move.sh.

---
+++ Game/game_state.sh
@@ -0,0 +1,38 @@
+#! /bin/bash
+
+# . ./grid.sh --source_only
+# . ./move.sh --source_only
+
+# new(row, column, level) {
+	# grid.generate_grid()
+	# grid.print_grid()
+	# continue()
+# }
+
+# continue() {
+	# grid.generate_grid()
+	# grid.print_grid()
+	# move.next_move()
+# }
+
+# lose() {
+	# echo "You've activated a mine. Game over."
+	# play_again()
+# }
+
+# win() {
+	# echo "Congratulations. Your field is safe."
+	# play_again()
+# }
+
+# play_again() {
+	# echo "War is hell."
+        # echo "Play again? (y/n)"
+        # read response
+
+        # if[ "$response" = y ]; then
+                # minesweeper.sh
+        # else
+                # SIGINT OR SIGKILL process
+        # fi
+# }
---
+++ minesweeper.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+# . ./Game/game_state.sh --source_only
+
+# echo "Enter row number: "
+# read row_number
+
+# echo "Enter column number: "
+# read column_number
+
+# echo "Enter difficultly (1-10): "
+# read level
+
+## Function imported from ./Game/game_state.sh
+# new(row_number, column_number, level)
