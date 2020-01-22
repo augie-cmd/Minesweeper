d3491cf95d44cef8bbcb45bfb211b3f6a25841a6
augie <8711710@gmail.com>
2020-01-21 19:59:28 -0800

    grid.sh Comments
    Initial grid generation process.

--- Game/grid.sh
+++ Game/grid.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 
-# Note: Consider using associative arrays.
 # Note: Scope issues.
 
 # generate_grid(row_number, column_number, level) {
@@ -21,4 +20,32 @@
 		# done
 # }
 
-# 
+# Place mines, record locations. Generate row arrays and
+# store row arrays inside master array.
+# 0's fill other positions.
+# Scan through arrays and replace 0 with 1 if position is
+# adjacent to mine. X symbolizes a mine.
+# Note: Scope is limited to using 1, for now. (As to say, if two
+# mines are next to each other, the overlapping flags
+# will only say 1 instead of 2.)
+# Example process:
+
+#   1 2 3 4
+#   -------
+# 1|1 1 1 0
+# 2|1 X 1 0
+# 3|1 1 1 0
+# 4|0 0 0 0
+
+# Mine Position (2,2)
+
+# (1,1): (2-1, 2-1)
+# (1,2): (2-1, 2-0)
+# (1,3): (2-1, 2+1)
+# -----------------
+# (2,1): (2-0, 2-1)
+# (2,3): (2-0, 2+1)
+# -----------------
+# (3,1): (2+1, 2-1)
+# (3,2): (2+1, 2-0)
+# (3,3): (2+1, 2+1)
