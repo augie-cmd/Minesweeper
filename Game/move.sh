c62b86476907309cb5532a63905fb8d3dc88d195
augie <8711710@gmail.com>
2020-02-27 18:45:21 -0800

    Organization Comments

--- Game/move.sh
+++ Game/move.sh
@@ -3,65 +3,54 @@
 # . ./game_state.sh --source_only
 # . ./grid.sh --source_only
 
-# Player has two options: uncover
-# or add a flag to coordinate entered.
+# Player has two options (if not win or lose:
+# uncover or add a flag to coordinate entered.
 
 next_move() {
 	echo "Select 1 or 2"
 	echo "1. Place flag; 2. Uncover (1/2): "
 	read move
 
-	# if[ "$move" = 1]; then
-		# ADD: Invalid placement check
-		# place_flag()
-	# fi
-
-	# if[ "$move" = 2]; then
-		# if[ win() = "false"] {
-			# uncover()
-		# }
-	#fi
-
 	case "$move" in
 		1 )
-			# place_flag()
-			echo "move 1"
+			place_flag
 			;;
 		2 )
-			# if[ win() = "false"] {
-				# uncover()
-			# }
 			echo "move 2"
 			;;
 		* )
 			echo "Invalid selection."
 			;;
 		esac
-	## If selection doesn't match
-		## echo "Invalid selection."
-	## call next_move()?
-	## current call preserved in stack?
-
 }
 
-# place_flag() {
+read_row() {
+	echo "Enter row (x):"
+	read row
+	echo "Enter column (y):"
+	read column
+}
+
+place_flag() {
 	## Place flag on grid unless location
 	## already has a flag or is uncovered.
 	## How? Unsure. (Color - display issue)
+	read_row
 
-	# echo "Enter row (x):"
-	# read row
-	# echo "Enter column (y):"
-	# read column
+	# ADD: Invalid flag placement check
+	# Add flag to grid printed to screen
+}
 
-	# add_flag_to_print_grid(row, column)
-# }
-
-# uncover() {
-	# echo "Enter row (x):"
-	# read row
-	# echo "Enter column (y):"
-	# read column
+uncover() {
+	read_row
 
 	# uncover_location_on_print_grid(row, column)
-# }
+
+	# ADD: Invalid uncover check
+	# 1. Convert alpha to int
+	# 2. Create 'row,column' string
+	# 3. Is 'row,colum' win or lose?
+	# 4. Process: If 0, uncover all touching 0s
+	# if num > 0, reveal single location
+	# continue/new_move
+}
